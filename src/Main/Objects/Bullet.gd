d7674d3cba664c2f4887e8deef9a1e9b9828b4c1
AldoFA22 <aldo.fa220698@gmail.com>
2022-10-28 03:28:28 -0500

    Added Rolling Enemy and Fixed Shooter Enemy

--- src/Main/Objects/Bullet.gd
+++ src/Main/Objects/Bullet.gd
@@ -4,13 +4,31 @@ var move = Vector2.ZERO
 # Detect player direction
 var lookVector = Vector2.ZERO
 var player = null
-var speed = 10
+var speed = 0
+var bulletInitialDistance = Vector2.ZERO
+var bulletFinalDistance = Vector2.ZERO
+
+onready var Asprite = $AnimatedSprite
 
 func _ready():
 	#Calculate distance between player and bullet position
 	lookVector = player.position - global_position
+	#Calculate initial position of bullet
+	bulletInitialDistance = global_position
 
 func _physics_process(delta):
+	speed += 0.05
+	
+	if lookVector.x >= 0:
+		Asprite.flip_h = true
+	else: Asprite.flip_h = false
+	
 	move = move.move_toward(lookVector, delta)
 	move = move.normalized() * speed
+	bulletFinalDistance = global_position - bulletInitialDistance
 	position += move
+	
+	if abs(bulletFinalDistance.x) > 1000 or abs(bulletFinalDistance.y) > 1000:
+		queue_free()
+	#print(bulletFinalDistance)
+	
---
+++ src/Main/Objects/RollingEnemy.gd
@@ -0,0 +1,39 @@
+extends "res://src/Actors/Actor.gd"
+
+var direction = Vector2.RIGHT #(0,1) * -1 = (-1,0)
+var velocity = 250
+var raycast_position = 31
+var flag = true
+
+onready var ledgeCheck = $LedgeCheck
+onready var Asprite = $AnimatedSprite
+
+var motion = Vector2()
+const up = Vector2(0,-1)
+
+func _physics_process(delta):
+	motion.y += 5
+	var found_wall = is_on_wall()
+	var found_floor = is_on_floor()
+	var found_ledge = not ledgeCheck.is_colliding()
+	
+	Asprite.play("walking")
+	#print(found_wall,found_ledge)
+	if found_wall or found_ledge and found_floor:
+		direction = -1
+		Asprite.flip_h = flag
+		if flag == true:
+			flag = false
+			ledgeCheck.position.x = raycast_position
+		else: 
+			flag = true
+			ledgeCheck.position.x = -raycast_position
+		motion.x = velocity
+		velocity = velocity * -1
+	elif found_floor == false:
+		motion.x = 0
+	else:
+		motion.x = velocity * -1
+		ledgeCheck.position.x = -raycast_position * -1
+		
+	move_and_slide(motion,up)
