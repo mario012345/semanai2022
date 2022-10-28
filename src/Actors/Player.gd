033ffb9b4fa9ad75a094f5697f583636ced381f9
Mario <A01732529@tec.mx>
2022-10-28 12:25:58 -0500

    Added Player sound

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -7,7 +7,6 @@ onready var platform_detector = $PlatformDetector
 onready var animated_sprite = $AnimatedSprite
 onready var weapon_timer = $WeaponTimer
 onready var weapon_collision = $Weapon.get_children()[0]
-# onready var sound_jump = $Jump
 
 var slice_vel = 200
 
@@ -71,13 +70,12 @@ func animation_process():
 			animated_sprite.flip_h = true
 
 func audio_process():
-	# Play jump sound
-	if Input.is_action_pressed("jump") and not $ExampleSoundPlayer.playing:
-		$ExampleSoundPlayer.play()
-	elif Input.is_action_pressed("attack") and is_attacking and not $ExampleSoundPlayer.playing:
-		$ExampleSoundPlayer.play()
-	elif (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")) and not $ExampleSoundPlayer.playing:
-		$ExampleSoundPlayer.play()
+	if Input.is_action_just_pressed("jump") and not $JumpAudio.playing:
+		$JumpAudio.play()
+	elif Input.is_action_just_pressed("attack") and is_attacking and not $HitAudio.playing:
+		$HitAudio.play()
+	#elif (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")) and not $ExampleSoundPlayer.playing:
+	#	$ExampleSoundPlayer.play()
 
 func get_direction():
 	return Vector2( Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), -1 if is_on_floor() and Input.is_action_just_pressed("jump") else 0)
